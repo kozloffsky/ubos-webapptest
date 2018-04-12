1225f97cea05531a427ea144887c90bea8877685
Russell Mooney <russellmooney999@gmail.com>
2018-04-12 17:52:08 +0300

    ported bin/webapptest script

--- ubos/commands.py
+++ ubos/commands.py
@@ -1,30 +1,77 @@
 from os import getcwd
 
-from ubos.webapptest import find_app_tests_in_directory
+import ubos.webapptest
 
 
 class Run(object):
-    pass
+
+    @classmethod
+    def run(cls, args):
+        print(args)
+
+    @classmethod
+    def synopsis_help(cls):
+        return {
+            "[--verbose | --logConfig <file>] [--interactive] [--scaffold <scaffold>] [--testplan <testplan>] [--tlskeyfile <tlskeyfile> --tlscrtfile <tlscrtfile>] <apptest>...": """
+       Run the test apptest.
+
+    --interactive: stop at important points and wait for user input
+
+    <scaffold>:   use this named scaffold instead of the default. If given as
+
+                  "abc:def=ghi:jkl=mno", "abc" represents the name of the scaffold,
+
+                  and "def" and "jkl" are scaffold-specific options
+
+    <testplan>:   use this named testplan instead of the default. If given as
+
+                  "abc:def=ghi:jkl=mno", "abc" represents the name of the testplan,
+
+                  and "def" and "jkl" are scaffold-specific options
+
+    <tlskeyfile>, <tlscrtfile>: files containing TLS key certificate, and
+
+                  all required certificates up the chain in one file if test
+
+                  is supposed to be run with TLS
+
+       """, "[--verbose | --logConfig <file>] --configfile <configfile> <apptest>...": """
+         Run the test apptest.
+
+    <configfile>: Read arguments from <configfile>, instead of from command-line
+
+                  arguments. If arguments are provided on the command-line
+
+                  anyway, they will override the values from the config file.
+
+                  The config file must be a JSON file, in a hierarchical order
+
+                  that corresponds to the command-line arguments and options for
+
+                  scaffolds and testplans.
+        """}
 
 
 class ListAppTests(object):
 
-    def run(self, *args, **kwargs):
+    @classmethod
+    def run(cls, args, **kwargs):
         app_tests = dict()
         if args:
             for directory in args:
-                tests = find_app_tests_in_directory(directory)
+                tests = ubos.webapptest.find_app_tests_in_directory(directory)
                 if len(app_tests) is 0:
                     app_tests = tests.copy()
                 else:
                     #merge two dicts
                     app_tests.update(tests.copy())
         else:
-            app_tests = find_app_tests_in_directory(getcwd())
+            app_tests = ubos.webapptest.find_app_tests_in_directory(getcwd())
 
         print(app_tests)
 
-    def synopsis_help(self):
+    @classmethod
+    def synopsis_help(cls):
         return {
             '[ <dir> ]...': """
     Lists the available app tests in the specified directories.
---
+++ web_app_test.py
@@ -0,0 +1,68 @@
+import sys
+
+from ubos.webapptest import find_commands
+
+def main(argv):
+    commands = find_commands()
+    if not argv:
+        synopsis_help_quit()
+
+    command = argv[0]
+    print(command)
+
+    if command == '--help' or command == '-h':
+        synopsis_help_quit(long=1)
+
+    try:
+        cls = commands[command]
+
+        for arg in argv:
+            if arg == '--help' or arg == '-h':
+                synopsis_help_quit(cmd=command, long=1)
+
+        print("Invoking webapptest command", cls)
+
+        result = cls.run(argv[1:])
+
+        sys.exit(not result)
+    except KeyError:
+        synopsis_help_quit(long=1)
+
+
+def synopsis_help_quit(cmd="", help_for=None, long=False):
+    commands = find_commands()
+    if help_for is None:
+        if long:
+            print("""
+            The central testing script for UBOS web application testing.
+It may be invoked in the following ways:""")
+        else:
+            print("Synopsis:\n")
+
+    for command in sorted(commands.keys()):
+        if not help_for or command is help_for:
+            cls = commands[command]
+            synopsis_help = cls.synopsis_help()
+
+            for synopsis in sorted(synopsis_help.keys()):
+                hlp = synopsis_help[synopsis]
+
+                print(cmd + " " + command + synopsis if synopsis else "" + "\n")
+
+                if long or help_for:
+                    print(hlp + "\n\n")
+
+    if not help_for:
+        print(cmd + "--help\n")
+        if long:
+            print("Display help text\n\n")
+
+        print(cmd+" <command> [<args>...] --help\n")
+
+        if long:
+            print("Display help text for this command.\n")
+
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
